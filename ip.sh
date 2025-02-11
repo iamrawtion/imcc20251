15b33de85906179bc2e9446b7bd4f01fb3ffd979
roshanugs <roshan.nagekar@ubiquity.com>
2025-02-11 10:57:48 +0530

    pushed to new branch

--- ip.sh
+++ ip.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#Collect the ip addresses
 PUBLIC_IP=$(curl -s https://ipinfo.io/ip)
 PRIVATE_IP=$(hostname -i | awk '{print $1}')
 
+#Print the IPs
 echo "Public IP: ${PUBLIC_IP}"
 echo "Private IP: ${PRIVATE_IP}"
